91a0308244b0210b7e7906138c337a5990c69c7c
Ricardo Gonzalez <rdo.gonzalez4@gmail.com>
2025-06-12 12:49:13 -0600

    Finish ejercicio Fizzbuzz

--- logica/fizz.js
+++ logica/fizz.js
@@ -1,3 +1,4 @@
+/*
 Reto de Programación: FizzBuzz
 Descripción:
 
@@ -10,3 +11,43 @@ Si el número es múltiplo de 5, imprime "Buzz".
 Si el número es múltiplo de 3 y 5, imprime "FizzBuzz".
 
 Si no es múltiplo de ninguno, imprime el número tal cual
+
+1.- 100 numeros
+2.- crear un bucle que recorra 100 numeros
+3.-Si el número es múltiplo de 3, imprime "Fizz".
+
+ 3.1- ocupar la expresion %3
+ 3.2.- es verdad si el modulo es = 0
+
+4.-Si el número es múltiplo de 5, imprime "Buzz".
+
+ 4.1- ocupar la expresion %5
+ 4.2.- es verdad si el modulo es = 0
+
+5.-Si el número es múltiplo de 3 y 5, imprime "FizzBuzz".
+
+ 5.1- ocupar la expresion numero %3 y modulo %5
+ 5.2.- es verdad solo y solo si ambas expresiones son = 0
+
+6.-Si no es múltiplo de ninguno, imprime el número tal cual
+n. fizz, buzz o fizzbuzz
+*/
+
+let counter = 1; 
+do {
+  if(counter %3 === 0 && counter %5 === 0){
+    console.log( `El numero ${counter} es FizzBuzz`);
+  }
+  else if ( counter %3 === 0) {
+    console.log (`El numero ${counter} es Fizz`);
+  }
+  else if ( counter %5 === 0){
+    console.log (`El numero ${counter} es Buzz`);
+  } else {
+    console.log( counter);
+  }
+
+  counter++;
+
+} while( counter <= 100)
+
